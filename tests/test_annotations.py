22b919beca725083db0f4fdf335e6ec74f7a6445
Robin De Schepper <robin.deschepper93@gmail.com>
2024-08-06 15:52:44 -0400

    change loading mechanism to see if CI errors disappear

--- tests/test_annotations.py
+++ tests/test_annotations.py
@@ -1,5 +1,7 @@
 import glob
+import os
 from pathlib import Path
+import importlib.util
 
 import pytest
 
@@ -8,7 +10,11 @@ import pytest
     "example", glob.glob(str(Path(__file__).parent.parent / "examples" / "*.py"))
 )
 def test_example_runnable(example):
-    # Load the example file
-    module = compile(open(example).read(), "test", "exec")
-    # Execute it to see if it throws any errors
-    exec(module)
+    # Load the example file as the starbars.__tests__.{name} module
+    name = example.split(os.sep)[-1].split(".")[0]
+    modname = f"starbars.__tests__.{name}"
+    spec = importlib.util.spec_from_file_location(modname, example)
+    module = importlib.util.module_from_spec(spec)
+
+    # Execute the example file
+    spec.loader.exec_module(module)
