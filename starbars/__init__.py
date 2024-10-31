1361b9c2f6c89b4fb92e1334ac84b87614c02a16
Elide <elide.98@gmail.com>
2024-10-31 12:30:35 +0100

    initial generalization refactor, broken results

--- starbars/__init__.py
+++ starbars/__init__.py
@@ -19,6 +19,7 @@ def draw_annotation(
     top_margin=0.05,
     text_distance=0.02,
     fontsize=10,
+    mode="vertical",
 ):
     """
     Draw statistical significance bars and p-value labels between chosen pairs of columns on existing plots.
@@ -36,45 +37,75 @@ def draw_annotation(
     if ax is None:
         ax = plt.gca()
 
+    if mode == "vertical":
+        annot_axis = 1
+        unit_vector = (0, 1)
+        get_lim = lambda: ax.get_ylim()
+        set_lim = lambda *args: ax.set_ylim(*args)
+    elif mode == "horizontal":
+        annot_axis = 0
+        unit_vector = (1, 0)
+        get_lim = lambda: ax.get_xlim()
+        set_lim = lambda *args: ax.get_xlim(*args)
+    else:
+        raise ValueError("mode must be either 'vertical' or 'horizontal' :)")
+
+    coords_to_px = create_coordinate_transformer(ax, mode)
+    px_to_coords = create_coordinate_transformer(ax, mode, True)
+
     dpi = ax.figure.dpi
     px_txt = (fontsize / 72) * dpi
-    px_ax = ax.transAxes.transform((0, 1))[1] - ax.transAxes.transform((0, 0))[1]
-    y = ax.get_ylim()[1]
-    y = ax.transData.inverted().transform(
-        (0, ax.transData.transform((0, y))[1] + px_ax * bar_gap)
-    )[1]
+    # Transform the annot axis interval from (0, 0) to (0, 1) into pixels
+    px_ax = (
+        ax.transAxes.transform(unit_vector)[annot_axis]
+        - ax.transAxes.transform((0, 0))[annot_axis]
+    )
+    # Get annot axis limit maximum
+    annot = get_lim()[1]
+    # Take annot axis limit maximum and add the first bar gap in axis pixels as starting point
+    annot = px_to_coords((0, coords_to_px((0, annot))[annot_axis] + px_ax * bar_gap))[
+        annot_axis
+    ]
 
     bars = []
     text_positions = []
     text_labels = []
 
     # Get the positions of the values
-    for x1, x2, pvalue in annotations:
+    for box1, box2, pvalue in annotations:
         label = pvalue_to_asterisks(pvalue)
         if label == "ns" and not ns_show:
             continue
-        x1_position, x2_position = get_positions(ax, x1, x2)
-        x1_px = ax.transData.transform((x1_position, y))[0]
-        x2_px = ax.transData.transform((x2_position, y))[0]
-        y_px = ax.transData.transform((x1_position, y))[1]
+        box1_position, box2_position = get_positions(ax, box1, box2, mode)
+        box1_px = coords_to_px((box1_position, annot))[+(not annot_axis)]
+        box2_px = coords_to_px((box2_position, annot))[+(not annot_axis)]
+        annot_px = coords_to_px((box1_position, annot))[annot_axis]
 
-        bar_x = [x1_px, x1_px, x2_px, x2_px]
-        bar_y = [y_px, px_ax * tip_length + y_px, px_ax * tip_length + y_px, y_px]
-        text_pos = ax.transData.inverted().transform(
-            ((x1_px + x2_px) / 2, px_ax * tip_length + y_px + px_ax * text_distance)
+        bar_box = [box1_px, box1_px, box2_px, box2_px]
+        bar_annot = [
+            annot_px,
+            px_ax * tip_length + annot_px,
+            px_ax * tip_length + annot_px,
+            annot_px,
+        ]
+        text_pos = px_to_coords(
+            (
+                (box1_px + box2_px) / 2,
+                px_ax * tip_length + annot_px + px_ax * text_distance,
+            )
         )
 
         points = [
-            ax.transData.inverted().transform((_x, _y)) for _x, _y in zip(bar_x, bar_y)
+            px_to_coords((_box, _annot)) for _box, _annot in zip(bar_box, bar_annot)
         ]
         bars.append(points)
         text_positions.append(text_pos)
         text_labels.append(label)
 
-        # Move up the y point to the next bar's start y
-        y = ax.transData.inverted().transform(
-            (x1_px, y_px + px_ax * tip_length + px_ax * bar_gap + px_txt)
-        )[1]
+        # Move up the annot point to the next bar's start annot
+        annot = px_to_coords(
+            (box1_px, annot_px + px_ax * tip_length + px_ax * bar_gap + px_txt)
+        )[annot_axis]
 
     # Draw the statistical annotation
     for bar, text_pos, label in zip(bars, text_positions, text_labels):
@@ -87,18 +118,37 @@ def draw_annotation(
             va="center",
             fontsize=fontsize,
             color="k",
+            rotation=90 * (mode == "horizontal"),
         )
 
     if len(annotations) == 0:
         return
 
-    # Adjust the y-axis limit of the current subplot to accommodate the top margin
-    y_px = ax.transData.transform((x1_position, y))[1]
-    y_final = ax.transAxes.inverted().transform((0, y_px + px_ax * top_margin))[1]
-    y = ax.transData.inverted().transform((0, ax.transAxes.transform((0, y_final))[1]))[
-        1
-    ]
+    # Adjust the annot axis limit of the current subplot to accommodate the top margin
+    annot_px = coords_to_px((box1_position, annot))[annot_axis]
+    annot_final = px_to_coords((0, annot_px + px_ax * top_margin))[annot_axis]
+    annot = px_to_coords((0, coords_to_px((0, annot_final))[annot_axis]))[annot_axis]
 
-    if y_final > 1:
-        y0, ym = ax.get_ylim()
-        ax.set_ylim(y0, y)
+    # If final annotation is out of bounds, adjust the annot limit to include it.
+    if annot_final > 1:
+        annot0, annot_max = get_lim()
+        set_lim(annot0, annot)
+
+
+def create_coordinate_transformer(ax, mode, inverse=False):
+    if inverse:
+        transformation = ax.transData.inverted().transform
+    else:
+        transformation = ax.transData.transform
+
+    if mode == "vertical":
+
+        def coords_to_px(coords):
+            return transformation(coords)
+
+    else:
+
+        def coords_to_px(coords):
+            return transformation(coords.reversed())
+
+    return coords_to_px
